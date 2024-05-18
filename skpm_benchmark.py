33492fb67a3bf97ce4c8f82911deba1ae1a4e9ac
Reza <reza.gholamrezazadeh@mindbridge.ai>
2024-05-18 19:03:16 -0400

    feature(main): add benchmark logic to test both aggregation and window aggregation functions.

--- skpm_benchmark.py
+++ skpm_benchmark.py
@@ -1,43 +1,29 @@
-from processing import agg_pandas, agg_polars
-from dataframe import get_df
+from helpers import percentage
+from processing import agg_pandas, agg_polars, win_agg_polars, win_agg_pandas
+from dataframe import get_df, get_timings, plot_timings
 
 
 def benchmark():
-    pd_df = get_df(engine="pandas")
-    pl_df = get_df(engine="polars")
-    agg_pandas(pd_df)
-    agg_polars(pl_df)
-    print("----------------------------")
-    # win_agg_pandas(pd_df.iloc[:200000])
-    # win_agg_polars(pl_df)
+    polars_df = get_df("polars")[:10000]
+    pandas_df = get_df("pandas")[:10000]
+    total = len(polars_df)
+    percentages = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+    # percentage_labels = [f'{p}({percentage(p, len(polars_df))})' for p in percentages]
+
+    # Get timings for Polars and Pandas
+    polars_timings = get_timings(polars_df, agg_polars, win_agg_polars, percentages)
+    pandas_timings = get_timings(pandas_df, agg_pandas, win_agg_pandas, percentages)
+
+    # Extract timings for plotting
+    polars_agg_times = [polars_timings[p][0] for p in percentages]
+    polars_win_agg_times = [polars_timings[p][1] for p in percentages]
+    pandas_agg_times = [pandas_timings[p][0] for p in percentages]
+    pandas_win_agg_times = [pandas_timings[p][1] for p in percentages]
+
+    # Plot timings
+    plot_timings(percentages, total, polars_agg_times, pandas_agg_times, polars_win_agg_times,
+                 pandas_win_agg_times)
 
 
 if __name__ == '__main__':
     benchmark()
-
-    #
-    #     df = pd.DataFrame({
-    #         elc.timestamp: np.arange(10),
-    #         elc.activity: np.random.randint(0, 10, 10),
-    #         elc.resource: np.random.randint(0, 3, 10),
-    #         elc.case_id: np.random.randint(0, 3, 10),
-    #     }).sort_values(by=[elc.case_id, elc.timestamp])
-    #     df = pd.get_dummies(df, columns=[elc.activity, elc.resource], dtype=int)
-    #     df = df.drop(elc.timestamp, axis=1)
-    #     agg = Aggregation(engine="pandas").fit_transform(df)
-    #     # print(infer_column_types(df))
-    #     print(agg)
-    #
-    #     pl_df = pl.DataFrame({
-    #         elc.timestamp: np.arange(10),
-    #         elc.activity: np.random.randint(0, 10, 10),
-    #         elc.resource: np.random.randint(0, 3, 10),
-    #         elc.case_id: np.random.randint(0, 3, 10),
-    #     }).sort(by=[elc.case_id, elc.timestamp])
-    #     pl_df = pl_df.to_dummies(columns=[elc.activity, elc.resource])
-    #     agg = Aggregation(engine="polars").fit_transform(pl_df)
-    # #     print(infer_column_types(df))
-    #
-    #     print(agg)
-    #
-    #
